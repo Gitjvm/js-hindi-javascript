096b2ddf7d5093c448fca23c33b84735f8c7de6b
Aakash Singh <124265895+Gitjvm@users.noreply.github.com>
2023-08-18 11:22:36 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let name = "vegita"
+let age =34
+let place = "plant vegita"
+
+//datatypes
+//number: 2 to power 53
+// boolean : true/ false
+// undefined : 
+// null : standalone value
+// BigInt
+// object :
+// Symbol : unique
+// object:
+
+console.log([typeof name, typeof age, typeof place]);
