197b5f866b01015065875985dc056d517d9aced6
Aakash Singh <124265895+Gitjvm@users.noreply.github.com>
2023-08-20 04:40:39 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+// operations
+// some common operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " itachi";
+
+let str3 = str1 + str2;
+console.log(str3);
